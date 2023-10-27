9225f0743d17d633c60c1c9b61343ef98376de42
mfcastilho <fredericocastilho@hotmail.com>
2023-10-26 21:37:19 -0300

    feat: create createUser

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -11,12 +11,12 @@ const userRepository = {
           return client.rows[0];
       },
        
-      findOne: (field, value)=>{
+      findOne: async (field, value)=>{
   
           const sql = `SELECT * FROM users WHERE ${field} = $1;`;
   
-          const client = connection.query(sql, [value]);
-  
+          const client = await connection.query(sql, [value]);
+
           return client.rows[0];
       },
   
@@ -25,13 +25,13 @@ const userRepository = {
           const { name, email, password } = data;
   
           const sql = `
-              INSERT INTO usuarios
+              INSERT INTO users
                   (name, email, password)
               VALUES
                   ($1, $2, $3) returning *;
           `;
   
-          const clientCreated = await connection.query(sql, [name, email, cryptedPassword]);
+          const clientCreated = await connection.query(sql, [name, email, password]);
   
           return clientCreated.rows[0];
       },
@@ -40,7 +40,7 @@ const userRepository = {
           const { name, email, password } = data;
   
           const sql = `
-              UPDATE usuarios
+              UPDATE users
               SET 
                   name = $1, 
                   email = $2, 
