799d49878a9ff13fe77c2f06aec05d49a7ffcfbb
mfcastilho <fredericocastilho@hotmail.com>
2023-10-27 23:30:57 -0300

    project finalized

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,20 +2,13 @@ const { Router } = require("express");
 
 const userRoutes = Router();
 
-<<<<<<< HEAD
 const { createUserController, loginController, updateUserController, getUserController } = require("../controllers/index");
 
 const { verifyEmail, verifyEmailFormat, verifyFields, verifyEmailLogin, verifyPasswordLogin, verifyToken } = require("../middlewares/index");
-=======
-const { createUserController } = require("../controllers/index")
-
-const { verifyEmail, verifyEmailFormat, verifyFields, verifyIfEmailNotExists } = require("../middlewares/index");
->>>>>>> b42208c474a7f42c43bc00c465cc99cb0be6abf1
 
 
 userRoutes.post("/usuario", verifyFields, verifyEmailFormat, verifyEmail, createUserController.handle);
 
-<<<<<<< HEAD
 userRoutes.post("/login", verifyFields, verifyEmailFormat, verifyEmailLogin, verifyPasswordLogin, loginController.handle);
 
 
@@ -25,6 +18,4 @@ userRoutes.put("/usuario", verifyToken, verifyFields, verifyEmailFormat, verifyE
 
 userRoutes.get("/usuario", verifyToken, getUserController.handle);
 
-=======
->>>>>>> b42208c474a7f42c43bc00c465cc99cb0be6abf1
 module.exports = userRoutes;
